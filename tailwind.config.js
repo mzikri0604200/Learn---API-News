0dabf7d8712f76a70b0ed23783a10d965e4d5536
mzikri0604200 <muhzikri06@gmail.com>
2023-01-27 16:41:27 +0700

    design by tailwindscss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xss: '0.5rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 };
