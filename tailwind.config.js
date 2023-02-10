ec082be28f65df9797bffbea4aa1498f06288479
mzikri0604200 <muhzikri06@gmail.com>
2023-02-10 17:13:49 +0700

    style hero news and add API news new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       fontSize: {
         xss: '0.5rem',
+      },
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
